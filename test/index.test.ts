1a1576cc7e259542b8a78f58a2173ea8a1b11d66
JudeBoy <judegiordano@gmail.com>
2022-01-30 22:49:32 -0500

    Database Mock Workflow

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,15 +1,20 @@
 /* eslint-disable no-undef */
 import { assert } from 'chai';
+import { Database } from './mock';
 
 before(async () => {
-	// await Seed();
+	await Database.Connect();
 });
 
 after(async () => {
-	// await Drop();
+	await Database.Close();
 });
 
 describe('generic test runner', () => {
+	it('should be conencted to database', async () => {
+		const connected = await Database.IsConnected();
+		assert.isTrue(connected);
+	});
 	it('should be a placeholder', async () => {
 		assert.equal(1 + 1, 2);
 	});
---
+++ test/mock/Database.ts
@@ -0,0 +1,23 @@
+import mongoose, { Mongoose, Connection } from 'mongoose';
+
+const connection = 'mongodb://localhost:27017/dev';
+
+export class Database {
+	private static mongooseInstance: Mongoose;
+	private static mongooseConnection: Connection;
+
+	public static async Connect(): Promise<Mongoose> {
+		if (Database.mongooseInstance) return Database.mongooseInstance;
+		Database.mongooseConnection = mongoose.connection;
+		Database.mongooseInstance = await mongoose.connect(connection);
+		return Database.mongooseInstance;
+	}
+
+	public static async Close() {
+		await Database.mongooseConnection.close();
+	}
+
+	public static IsConnected() {
+		return !!Database.mongooseConnection.db;
+	}
+}
