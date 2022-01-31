c9f1662652c61e8afb58ff62e5c584b1d043d37c
JudeBoy <judegiordano@gmail.com>
2022-01-30 22:34:32 -0500

    Init Ttes Runner (WIP)

---
+++ test/index.test.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import { assert } from 'chai';
+
+before(async () => {
+	// await Seed();
+});
+
+after(async () => {
+	// await Drop();
+});
+
+describe('generic test runner', () => {
+	it('should be a placeholder', async () => {
+		assert.equal(1 + 1, 2);
+	});
+});
