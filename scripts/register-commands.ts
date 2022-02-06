8df6331262204adf61fa979a71bc06889a9db855
judegiordano <judegiordano@gmail.com>
2022-02-06 12:33:20 -0500

    Register Commands Through Workflow Script

--- scripts/register-commands.ts
+++ scripts/register-commands.ts
@@ -1,1 +1,30 @@
-console.log('this is an example script :)');
+import { Collection } from 'discord.js';
+import { promisify } from 'util';
+import glob from 'glob';
+import path from 'path';
+
+import { CommandBuilder } from '../src/Helpers';
+import { ICommand, IExport } from '../src/Types';
+
+const globPromise = promisify(glob);
+
+async function main() {
+	const commands = new Collection<string, ICommand>();
+
+	const folder = path.resolve(`${__dirname}/../src/Commands/*{.js,.ts}`);
+	const files = await globPromise(folder);
+
+	for (const file of files) {
+		const { default: data } = await import(file) as IExport<ICommand>;
+		commands.set(data.customID, data);
+	}
+	console.log(`registering {${files.length}} commands`);
+
+	await CommandBuilder.RegisterCommands(commands);
+}
+
+try {
+	main();
+} catch (error) {
+	console.log(error);
+}
