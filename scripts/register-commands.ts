27b33a949a8884d625b9d65c3195d84332e82286
judegiordano <judegiordano@gmail.com>
2022-02-06 16:40:46 -0500

    Remove Unused Debug

--- scripts/register-commands.ts
+++ scripts/register-commands.ts
@@ -1,8 +1,5 @@
 import { CommandBuilder } from '../src/Helpers';
 import { HandlerType, ICommand } from '../src/Types';
-import debug from 'debug';
-
-const logSystem = debug('cordle:scripts');
 
 async function main() {
 	const commands = await CommandBuilder.ImportFiles<ICommand>(HandlerType.Commands);
