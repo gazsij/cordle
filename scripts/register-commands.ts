64245bce5f650e08e58633c132b48b1a9b10cb55
judegiordano <judegiordano@gmail.com>
2022-02-06 16:31:56 -0500

    Cleanup Register Script

--- scripts/register-commands.ts
+++ scripts/register-commands.ts
@@ -1,26 +1,12 @@
-import { Collection } from 'discord.js';
-import { promisify } from 'util';
-import glob from 'glob';
-import path from 'path';
-
 import { CommandBuilder } from '../src/Helpers';
-import { ICommand, IExport } from '../src/Types';
+import { HandlerType, ICommand } from '../src/Types';
+import debug from 'debug';
 
-const globPromise = promisify(glob);
+const logSystem = debug('cordle:scripts');
 
 async function main() {
-	const commands = new Collection<string, ICommand>();
-
-	const folder = path.resolve(`${__dirname}/../src/Commands/*{.js,.ts}`);
-	const files = await globPromise(folder);
-
-	for (const file of files) {
-		const { default: data } = await import(file) as IExport<ICommand>;
-		commands.set(data.customID, data);
-	}
-	console.log(`registering {${files.length}} commands`);
-	const done = await CommandBuilder.RegisterCommands(commands);
-	console.log(`successfully loaded {${done}} commands`);
+	const commands = await CommandBuilder.ImportFiles<ICommand>(HandlerType.Commands);
+	await CommandBuilder.RegisterCommands(commands);
 }
 
 try {
